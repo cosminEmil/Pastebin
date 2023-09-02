22022e5d53591ade72aeeef2c8d773d1452a1c42
cosminEmil <132258074+cosminEmil@users.noreply.github.com>
2023-09-02 11:54:38 +0300

    Update pastebin.js

--- pastebin.js
+++ pastebin.js
@@ -2,7 +2,6 @@ function addText() {
     let text = document.getElementById("textBox").value;
     let list = document.getElementById("myList");
     let link = document.createElement("a");
-    link.style.maxWidth = "20ch";
     let li = document.createElement("li");
     link.setAttribute('href', "pastebin-display.html");
     link.setAttribute("target", "_blank");
